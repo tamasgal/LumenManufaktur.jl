d58a2bfbcacf7924b8c40ea7188254363be5b9d9
Tamas Gal <himself@tamasgal.com>
2024-05-03 09:50:01 +0200

    Preliminary API

--- src/MuonLight.jl
+++ src/MuonLight.jl
@@ -4,14 +4,81 @@ using BasicInterpolators
 using FastGaussQuadrature
 
 export directlight
+export BasicDispersion
+export DispersionORCA
+export DispersionARCA
 
 abstract type MediumProperties end
 abstract type DispersionModel end
 
-struct PMTProperties
+
+struct PMTModel{T1, T2}
     photocathode_area::Float64
-    angular_acceptance::Float64
+    quantum_efficiency::T1
+    angular_acceptance::T2
 end
+
+# abstract type PMTModel end
+# function quantumefficiency end
+# photocathodearea(::Type{KM3NeTPMT}) = 45.4e-4
+# quantumefficiency(::Type{KM3NeTPMT}) = LinearInterpolator(
+#         [0, 270, 275, 280, 285, 290, 295, 300, 305, 310, 315, 320, 325, 330, 335,
+#         340, 345, 350, 355, 360, 365, 370, 375, 380, 385, 390, 395, 400, 405, 410,
+#         415, 420, 425, 430, 435, 440, 445, 450, 455, 460, 465, 470, 475, 480, 485,
+#         490, 495, 500, 505, 510, 515, 520, 525, 530, 535, 540, 545, 550, 555, 560,
+#         565, 570, 575, 580, 585, 590, 595, 600, 605, 610, 615, 620, 625, 630, 635,
+#         640, 645, 650, 655, 660, 665, 670, 675, 680, 685, 690, 695, 700, 705, 710,
+#         999999],
+#         # collection efficiency (with factor 0.9) correction
+#         0.01 * 0.9 * [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.1, 0.9, 1.8, 4.6, 7.4,
+#         11., 15., 18., 21., 22., 23., 24., 25., 25., 26., 26., 26., 26., 27., 26.,
+#         26., 26., 26., 26., 25., 25., 25., 25., 24., 24., 24., 23., 22., 22., 21.,
+#         20., 19., 19., 19., 18., 18., 18., 17., 16., 15., 14., 12., 11., 10., 9.5,
+#         8.7, 8.1, 7.6, 7.2, 6.8, 6.4, 6.0, 5.6, 5.1, 4.8, 4.4, 4.0, 3.6, 3.3, 3.0,
+#         2.7, 2.3, 2.1, 1.9, 1.7, 1.5, 1.4, 1.2, 1.0, 0.9, 0.7, 0.6, 0.5, 0.4, 0.3,
+#         0.2, 0.1, 0.0, 0.0],
+#         NoBoundaries()
+# )
+
+const PMTKM3NeT = PMTModel(
+    45.4e-4,
+    # Yields slightly different values compared to the Jpp getQE() function,
+    # very probably due to the interpolation implementation
+    LinearInterpolator(
+        [0, 270, 275, 280, 285, 290, 295, 300, 305, 310, 315, 320, 325, 330, 335,
+        340, 345, 350, 355, 360, 365, 370, 375, 380, 385, 390, 395, 400, 405, 410,
+        415, 420, 425, 430, 435, 440, 445, 450, 455, 460, 465, 470, 475, 480, 485,
+        490, 495, 500, 505, 510, 515, 520, 525, 530, 535, 540, 545, 550, 555, 560,
+        565, 570, 575, 580, 585, 590, 595, 600, 605, 610, 615, 620, 625, 630, 635,
+        640, 645, 650, 655, 660, 665, 670, 675, 680, 685, 690, 695, 700, 705, 710,
+        999999],
+
+        # collection efficiency (with factor 0.9) correction
+        0.01 * 0.9 * [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.1, 0.9, 1.8, 4.6, 7.4,
+        11., 15., 18., 21., 22., 23., 24., 25., 25., 26., 26., 26., 26., 27., 26.,
+        26., 26., 26., 26., 25., 25., 25., 25., 24., 24., 24., 23., 22., 22., 21.,
+        20., 19., 19., 19., 18., 18., 18., 17., 16., 15., 14., 12., 11., 10., 9.5,
+        8.7, 8.1, 7.6, 7.2, 6.8, 6.4, 6.0, 5.6, 5.1, 4.8, 4.4, 4.0, 3.6, 3.3, 3.0,
+        2.7, 2.3, 2.1, 1.9, 1.7, 1.5, 1.4, 1.2, 1.0, 0.9, 0.7, 0.6, 0.5, 0.4, 0.3,
+        0.2, 0.1, 0.0, 0.0],
+
+        NoBoundaries()
+    ),
+
+    # using eps(), max angle and two zero values in the second vector to
+    # get real zeros outside of the upper boundary defined in Jpp (>=0.4)
+    LinearInterpolator(
+        [-1.00, -0.95, -0.90, -0.85, -0.80, -0.75, -0.70, -0.65, -0.60, -0.55,
+        -0.50, -0.45, -0.40, -0.35, -0.30, -0.25, -0.20, -0.15, -0.10, -0.05, 0.00,
+        0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.40+eps(), 1.0],
+
+        [1.621, 1.346, 1.193, 1.073, 0.973, 0.877, 0.790, 0.711, 0.640, 0.575,
+        0.517, 0.450, 0.396, 0.341, 0.295, 0.249, 0.207, 0.166, 0.128, 0.095, 0.065,
+        0.038, 0.017, 0.006, 0.003, 0.002, 0.001, 0.001, 0.001, 0.000, 0.000],
+
+        NoBoundaries()
+    )
+)
 
 
 const α = 1.0/137.036
@@ -19,16 +86,20 @@ const dx = π / 25
 const wmin = 300
 const wmax = 700
 
-@Base.kwdef struct Dispersion
-    P::Float64 = 240               #  ambient pressure [atm]
+Base.@kwdef struct BasicDispersion <: DispersionModel
+    P::Float64 = 1                 #  ambient pressure [atm]
     a0::Float64 = 1.3201           #  offset
     a1::Float64 = 1.4e-5           #  dn/dP
     a2::Float64 = 16.2566          #  d^1n/(dx)^1
     a3::Float64 = -4383.0          #  d^2n/(dx)^2
     a4::Float64 = 1.1455e6         #  d^3n/(dx)^3
 end
+BasicDispersion(P) = BasicDispersion(P=P)
+const DispersionORCA = BasicDispersion(240)
+const DispersionARCA = BasicDispersion(350)
 
-@Base.kwdef struct Parameters
+
+Base.@kwdef struct Parameters
     minimum_distance::Float64 = 1.0e-1
     lambda_min::Float64 = 300
     lambda_max::Float64 = 700
@@ -36,16 +107,12 @@ end
 
 @inline getRmin() = 1.0e-1
 @inline getPhotocathodeArea() = 45.4e-4
-@inline function getIndexOfRefractionPhase(dp::Dispersion, λ)
+@inline function refractionindexphase(λ, dp::DispersionModel=BasicDispersion())
     x = 1.0 / λ
-    @show dp.a0
-    @show dp.a1
-    @show dp.a2
-    @show dp.a3
-    @show dp.a4
-    @show dp.P
-    @show λ
-    return dp.a0  +  dp.a1*dp.P  +  x*(dp.a2 + x*(dp.a3 + x*dp.a4))
+    dp.a0  +  dp.a1*dp.P  +  x*(dp.a2 + x*(dp.a3 + x*dp.a4))
+end
+@inline function refractionindexgroup(λ, dp::DispersionModel=BasicDispersion())
+    error("Not implemented yet")
 end
 
 """
@@ -60,7 +127,7 @@ absorptionlength = LinearInterpolator(
 )
 
 """
-    function scatteringlength(λ)
+    scatteringlength(λ)
 
 Calculates the scattering length [m] for a given wavelength `λ` [ns] using the
 Kopelevich model for spectral volume scattering functions. The model separates
@@ -90,42 +157,6 @@ function scatteringlength(λ)
 end
 
 
-# Yields slightly different values compared to the Jpp getQE() function,
-# very probably due to the interpolation implementation
-quantumefficiency = LinearInterpolator(
-    [0, 270, 275, 280, 285, 290, 295, 300, 305, 310, 315, 320, 325, 330, 335,
-    340, 345, 350, 355, 360, 365, 370, 375, 380, 385, 390, 395, 400, 405, 410,
-    415, 420, 425, 430, 435, 440, 445, 450, 455, 460, 465, 470, 475, 480, 485,
-    490, 495, 500, 505, 510, 515, 520, 525, 530, 535, 540, 545, 550, 555, 560,
-    565, 570, 575, 580, 585, 590, 595, 600, 605, 610, 615, 620, 625, 630, 635,
-    640, 645, 650, 655, 660, 665, 670, 675, 680, 685, 690, 695, 700, 705, 710,
-    999999],
-
-    # collection efficiency (factor 0.9) correction
-    0.01 * 0.9 * [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.1, 0.9, 1.8, 4.6, 7.4,
-    11., 15., 18., 21., 22., 23., 24., 25., 25., 26., 26., 26., 26., 27., 26.,
-    26., 26., 26., 26., 25., 25., 25., 25., 24., 24., 24., 23., 22., 22., 21.,
-    20., 19., 19., 19., 18., 18., 18., 17., 16., 15., 14., 12., 11., 10., 9.5,
-    8.7, 8.1, 7.6, 7.2, 6.8, 6.4, 6.0, 5.6, 5.1, 4.8, 4.4, 4.0, 3.6, 3.3, 3.0,
-    2.7, 2.3, 2.1, 1.9, 1.7, 1.5, 1.4, 1.2, 1.0, 0.9, 0.7, 0.6, 0.5, 0.4, 0.3,
-    0.2, 0.1, 0.0, 0.0],
-
-    NoBoundaries()
-)
-
-angularacceptance = LinearInterpolator(
-
-    [-1.00, -0.95, -0.90, -0.85, -0.80, -0.75, -0.70, -0.65, -0.60, -0.55,
-    -0.50, -0.45, -0.40, -0.35, -0.30, -0.25, -0.20, -0.15, -0.10, -0.05, 0.00,
-    0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40],
-
-    [1.621, 1.346, 1.193, 1.073, 0.973, 0.877, 0.790, 0.711, 0.640, 0.575,
-    0.517, 0.450, 0.396, 0.341, 0.295, 0.249, 0.207, 0.166, 0.128, 0.095, 0.065,
-    0.038, 0.017, 0.006, 0.003, 0.002, 0.001, 0.001, 0.001],
-
-    WeakBoundaries()
-)
-
 """
     cherenkov(λ, n)
 
@@ -145,53 +176,63 @@ end
 
 
 """
-    directlight(dp::Dispersion, R, θ, ϕ)
+    directlight(dp::DispersionModel, pmt::PMTModel, R, θ, ϕ)
 
 Returns the number of photo-electrons from direct Cherenkov light from a muon
 with a distance of `R` [m] to the PMT and the angles `θ` [rad] (zenith) and `ϕ`
 [rad] (azimuth) with respect to the PMT axis.
 
+# Arguments
+
+- `ϕ`: the zenith angle [rad] which is 0 when the PMT points away from the muon
+  track (in x-direction) and rotates counter clockwise to the y-axis when
+  viewed from above, while the z-axis points upwards.
+- `θ`: the azimuth angle [rad] which is 0 when the PMT points upwards (along the
+  z-axis) and π/2 when pointing downwards. Between 0 and π/2, the PMT points to
+  the z-axis
+
+
 """
-function directlight(dp::Dispersion, R, θ, ϕ)
+function directlight(dp::DispersionModel, pmt::PMTModel, R, θ, ϕ)
     value = 0
 
 
     R  =  max(R, getRmin())
-    A  =  getPhotocathodeArea()
+    A  =  pmt.photocathode_area
 
     px =  sin(θ)*cos(ϕ)
     pz =  cos(θ)
 
     # @show px
     # @show pz
-    @show wmin
-    @show wmax
+    # @show wmin
+    # @show wmax
 
     # for (m_x, m_y) in [(cos(x), sin(x)) for x in 0.5dx:dx:π]
     for (m_x, m_y) in zip(gausslegendre(5)...)
 
-      println("-----------")
-      @show m_x
-      @show m_y
+      # println("-----------")
+      # @show m_x
+      # @show m_y
 
       w  = 0.5 * (wmax + wmin)  +  m_x * 0.5 * (wmax - wmin)
-      @show w
+      # @show w
 
       dw = m_y * 0.5 * (wmax - wmin)
-      @show dw
+      # @show dw
 
-      n     = getIndexOfRefractionPhase(dp, w)
-      @show n
+      n     = refractionindexphase(w, dp)
+      # @show n
 
       l_abs = absorptionlength(w);
-      @show l_abs
+      # @show l_abs
       ls    = scatteringlength(w);
-      @show ls
+      # @show ls
 
-      @show cherenkov(w,n)
-      @show quantumefficiency(w)
-      npe   = cherenkov(w,n) * dw * quantumefficiency(w);
-      @show npe
+      # @show cherenkov(w,n)
+      # @show PMTKM3NeT.quantum_efficiency(w)
+      npe   = cherenkov(w,n) * dw * pmt.quantum_efficiency(w);
+      # @show npe
 
       ct0 = 1.0 / n;
       st0 = sqrt((1.0 + ct0)*(1.0 - ct0));
@@ -199,7 +240,7 @@ function directlight(dp::Dispersion, R, θ, ϕ)
       d  = R / st0;                                 # distance traveled by photon
       ct = st0*px + ct0*pz;                         # cosine angle of incidence on PMT
 
-      U  = angularacceptance(ct);                # PMT angular acceptance
+      U  = pmt.angular_acceptance(ct);                # PMT angular acceptance
       V  = exp(-d/l_abs) * exp(-d/ls);              # absorption & scattering
       W  = A / (2.0*π*R*st0);                      # solid angle
 
