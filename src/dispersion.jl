98d407379215f5b6e10c6e11a1d93ef6db9e5482
Tamas Gal <himself@tamasgal.com>
2024-05-10 12:04:51 +0200

    Add wavelength() for dispersion

--- src/dispersion.jl
+++ src/dispersion.jl
@@ -8,6 +8,9 @@ Light dispersion model for water in deep sea based on David J.L. Bailey's work:
 and predicting its sensitivity to Dark Matter" - PhD thesis, University of
 Oxford, United Kingdom, 2002.
 
+The Bailey dispersion interface is based on the implementation in the Jpp
+framework by Maarten de Jong.
+
 """
 Base.@kwdef struct BaileyDispersion <: DispersionModel
     P::Float64 = 1                 #  ambient pressure [atm]
@@ -48,4 +51,30 @@ end
     ng  = n / (1.0 + np*λ/n)
 
     ng^2 * (2*np^2 - n*npp) * λ / (n^3);
+end
+
+"""
+Determine wavelength [nm] for a given index of refraction corresponding to the
+group velocity. The estimate of the wavelength is made by successive linear
+extrapolations. The procedure starts from the given wavelength and
+terminates if the index of refraction is equal to the target value within
+the given precision.
+
+# Arguments
+
+- `n`: of refraction
+- `w`: value wavelength [nm]
+- `eps`: precision index of refraction
+
+"""
+function wavelength(dp::BaileyDispersion, n, w, eps)
+    v = w
+
+    while true
+      y = refractionindexgroup(dp, v)
+      abs(y - n) < eps && break
+      v += (n - y) / dispersiongroup(dp, v)
+    end
+
+    v
 end
